39588c4a135c6bac17e804505cb61b2a07178b32
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2016-07-26 16:03:06 +0200

    This should pass

--- test/grafterdatacube/test.clj
+++ test/grafterdatacube/test.clj
@@ -4,15 +4,15 @@
             [grafterdatacube.core :refer :all])
   )
 
-(deftest test-fails
-  (testing "I fail."
-    (is (= 1 0))))
+;(deftest test-fails
+;  (testing "I fail."
+;    (is (= 1 0))))
 
 (deftest test-passes
   (testing "I pass."
     (is (= 0 0))))
 
 (deftest add-x-to-y
-  (is (= 6 (add 2 3))))
+  (is (= 5 (add 2 3))))
 
 (run-tests)
